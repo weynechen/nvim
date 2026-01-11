fe19e7d34b5e5ed9365a875c846d5275029dd683
weynechen <weynechen@qq.com>
2026-01-11 08:47:13 +0800

    fix: update lazy-lock.json dependencies and modify telescope configuration for fzf-native build command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,5 @@
 -- Telescope - Fuzzy finder (like Ctrl+P in VS Code)
 
--- Cross-platform build command for fzf-native
-local function fzf_native_build()
-  if vim.fn.has("win32") == 1 then
-    -- Windows: use cmake, then copy DLL to expected location
-    return "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && copy build\\Release\\libfzf.dll build\\"
-  else
-    -- Linux/macOS: use make
-    return "make"
-  end
-end
-
 return {
   "nvim-telescope/telescope.nvim",
   branch = "master",
@@ -18,7 +7,7 @@ return {
     "nvim-lua/plenary.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
-      build = fzf_native_build(),
+      build = "make", -- windows need to install make
     },
   },
   config = function()
@@ -53,6 +42,14 @@ return {
         },
       },
     })
-    telescope.load_extension("fzf")
+    -- fzf extension: requires compilation (cmake/make/zig)
+    -- If not available, Telescope will use built-in Lua matcher (slower but works)
+    local ok, err = pcall(telescope.load_extension, "fzf")
+    if not ok then
+      vim.notify(
+        "telescope-fzf-native failed to load. Using built-in matcher.\nInstall 'zig' and run :Lazy build telescope-fzf-native.nvim",
+        vim.log.levels.WARN
+      )
+    end
   end,
 }
